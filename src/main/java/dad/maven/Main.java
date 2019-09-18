c41ab9916eb850bd8ffeba66439efae7b3b16d89
MichaelTP <michaeljtp1@gmail.com>
2019-09-18 11:03:24 +0100

    first commit

---
+++ src/main/java/dad/maven/Main.java
@@ -0,0 +1,12 @@
+package dad.maven;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		String invertido=StringUtils.reverse("hola Maven!");
+		System.out.println(invertido);
+	}
+
+}
